71fc7dce715f4cb5735a290306d947acf46897e7
lweston <leighweston@Leighs-MBP.dhcp.lbl.gov>
2018-10-03 18:24:35 -0700

    add predict() class method

--- model/tf_model.py
+++ model/tf_model.py
@@ -4,8 +4,13 @@ import math
 import numpy as np
 
 class ConvNet:
-    def __init__(self):
-        self.model = None
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.X = tf.placeholder(dtype=tf.float32, shape = input_shape)
+        self.y = tf.placeholder(dtype=tf.float32, shape = output_shape)
+        self.sess = tf.Session()
+        self.logits = None
 
     def initialize_filters(self):
         '''Initialize weights on filters
@@ -21,7 +26,7 @@ class ConvNet:
 
         return W1, W2
 
-    def forward_prop(self, X, W1, W2, num_classes):
+    def forward_prop(self, X, W1, W2):
         '''Forward propagation. The model has two conv2D layers
         followed by two fully connected layers.
         '''
@@ -46,6 +51,7 @@ class ConvNet:
         A4 = tf.contrib.layers.fully_connected(inputs = A3, num_outputs=50, activation_fn=tf.nn.relu)
 
         #Output layer
+        num_classes = self.output_shape[-1]
         Z5 = tf.contrib.layers.fully_connected(inputs = A4, num_outputs = num_classes, activation_fn = None)
 
         return Z5
@@ -56,54 +62,50 @@ class ConvNet:
         y_batches = np.array_split(y, num_batches, axis=0)
         return [(X_batch, y_batch) for X_batch, y_batch in zip(X_batches, y_batches)]
 
-    def fit_model(self, X_train, y_train, X_test, y_test, learning_rate = 0.001, num_epochs = 5, minibatch_size = 64):
-
-        #Create placeholders
-        n_samples, n_H0, n_W0, n_C0 = X_train.shape
-        n_samples, n_y = y_train.shape
-        X = tf.placeholder(dtype=tf.float32, shape = [None, n_H0, n_W0, n_C0])
-        y = tf.placeholder(dtype=tf.float32, shape = [None, n_y])
+    def fit_model(self, X_train, y_train, learning_rate = 0.001, num_epochs = 5, minibatch_size = 64):
 
         #Initialize filters
         W1, W2 = self.initialize_filters()
 
         #Forward propagation
-        Z5 = self.forward_prop(X, W1, W2, n_y)
+        self.logits = self.forward_prop(self.X, W1, W2)
 
         #Cost function and optimizer
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z5, labels=y))
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y))
         optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
         init = tf.global_variables_initializer()
 
-        #Start a tf session
-        with tf.Session() as sess:
-
-            sess.run(init)
-
-            for epoch in range(num_epochs):
-                epoch_cost = 0
-                minibatches = self.get_minibatches(X_train, y_train, minibatch_size)
-                num_batches = len(minibatches)
-                for minibatch in minibatches:
-                    batch_X, batch_y = minibatch
-                    _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X:batch_X, y:batch_y})
-                    epoch_cost += minibatch_cost
-                epoch_cost /= num_batches
-                print('Cost for epoch {}: {}'.format(epoch, epoch_cost))
-
-            pred = tf.nn.softmax(Z5)
-            accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1)), 'float'))
-            print(accuracy.eval({X: X_test, y: y_test}))
+        #Fit the model
+        self.sess.run(init)
+        for epoch in range(num_epochs):
+            epoch_cost = 0
+            minibatches = self.get_minibatches(X_train, y_train, minibatch_size)
+            num_batches = len(minibatches)
+            for minibatch in minibatches:
+                batch_X, batch_y = minibatch
+                _, minibatch_cost = self.sess.run([optimizer, cost], feed_dict={self.X:batch_X, self.y:batch_y})
+                epoch_cost += minibatch_cost
+            epoch_cost /= num_batches
+            print('Cost for epoch {}: {}'.format(epoch, epoch_cost))
 
 
-    def predict(self): pass
+    def assess(self, X_test, y_test):
+        pred = tf.nn.softmax(self.logits)
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(self.y, 1)), 'float'))
+        return accuracy.eval({self.X: X_test, self.y: y_test}, session = self.sess)
 
 
 if __name__ == '__main__':
     #Get the data
     X_train, X_test, y_train, y_test = get_data()
-    model = ConvNet()
-    model.fit_model(X_train, y_train, X_test, y_test)
+    n_data, n_H, n_W, n_C = X_train.shape
+    n_data, n_labels = y_train.shape
+    input_shape = [None, n_H, n_W, n_C]
+    output_shape = [None, n_labels]
+    model = ConvNet(input_shape, output_shape)
+    model.fit_model(X_train, y_train)
+    print(model.assess(X_test, y_test))
+
 
 
